e62ef2e7ac856752962fafd38ac1ddf8ba8c7248
Akshat Shah <aksshah2004@gmail.com>
2024-08-08 07:27:20 -0500

    add env variables properly to backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,10 @@ require('dotenv').config()
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI)
+        const conn = await mongoose.connect(process.env.MONGODB_URI)
         console.log(`MongoDB Connected: ${ conn.connection.host }`)
     } catch (error) {
+        console.log(`Error connecting to MongoDB: ${ error.message }`)
         process.exit(1)
     }
 }
