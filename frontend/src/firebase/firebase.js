0a96ca4fd9b04bac187f342e66cd7e8a62fcd3de
Ayuj Verma <ayuj@utexas.edu>
2024-08-10 10:05:17 -0500

    Added firebase config back.

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -7,7 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyCEA2h3857keqif3-s33R1M3hSJGmgrh10",
+  authDomain: "tindora-c728d.firebaseapp.com",
+  projectId: "tindora-c728d",
+  storageBucket: "tindora-c728d.appspot.com",
+  messagingSenderId: "1082066929556",
+  appId: "1:1082066929556:web:b7c190561ff90c7892b181",
+  measurementId: "G-045VNNRPY1"
 };
 
 // Initialize Firebase
